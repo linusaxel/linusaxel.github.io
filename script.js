5c9437f3d7f59cabc345ed117ada7a986b45ff0e
linusaxel <46885991+linusaxel@users.noreply.github.com>
2021-06-28 16:25:38 +0200

    finish json thing

--- script.js
+++ script.js
@@ -12,32 +12,66 @@ var text = `{
   }
 }`;
 
-var resume = JSON.parse(this.text);
-
 window.onload = (event) => {
-  var content = document.getElementById('content');
-  for (let [key, value] of Object.entries(this.resume)) {
-    var innerDiv = document.createElement("div");
-    innerDiv.textContent = key + ': ' + value + ',';
-    content.appendChild(innerDiv);
-    console.log(`${key}: ${value}`);
-  }
-
   //setClickListeners();
 };
 
-function setClickListeners() {
-  const clickableElements = document.getElementsByClassName("clickable");
-  Array.from(clickableElements).forEach((el) => {
-    el.onclick = () => {
-      const node = el.nextSibling;
-      if (node.style && node.style.display == "none") {
-        node.style.display = "block";
-        el.innerText = " -";
-      } else if (node.style && node.style.display == "block") {
-        node.style.display = "none";
-        el.innerText = "+";
-      }
-    };
-  });
+function minimizeMain() {
+  var button = document.getElementById('main-minimizable-button');
+  var parentElement = document.getElementById('parent-element');
+
+  if (button.classList.contains("fa-plus-square")) {
+    button.classList.remove("fa-plus-square");
+    button.classList.add("fa-minus-square");
+    parentElement.innerHTML = "{ <button type=\"button\" id=\"main-minimizable-button\" onclick=\"minimizeMain()\" class=\"far fa-minus-square json-button\"></button>";
+  } else if (button.classList.contains("fa-minus-square")){
+    button.classList.remove("fa-minus-square");
+    button.classList.add("fa-plus-square");
+    parentElement.innerHTML = "{ <button type=\"button\" id=\"main-minimizable-button\" onclick=\"minimizeMain()\" class=\"far fa-plus-square json-button\"></button> }";
+  }
+  toggleMinimizeElements("minimizable");
 }
+
+function minimizeHobbies() {
+  var button = document.getElementById("hobbies-minimizable-button");
+  var parentElement = document.getElementById("hobbies-element");
+
+  if (button.classList.contains("fa-plus-square")) {
+    button.classList.remove("fa-plus-square");
+    button.classList.add("fa-minus-square");
+    parentElement.innerHTML = "\"hobbies\": [ <button type=\"button\" id=\"hobbies-minimizable-button\" onclick=\"minimizeHobbies()\" class=\"far fa-minus-square json-button\"></button>";
+  } else if (button.classList.contains("fa-minus-square")){
+    button.classList.remove("fa-minus-square");
+    button.classList.add("fa-plus-square");
+    parentElement.innerHTML = "\"hobbies\": [ <button type=\"button\" id=\"hobbies-minimizable-button\" onclick=\"minimizeHobbies()\" class=\"far fa-plus-square json-button\"></button> ],";
+  }
+  toggleMinimizeElements("minimizable-hobbies")
+}
+
+function minimizeContacts() {
+  var button = document.getElementById("contacts-minimizable-button");
+  var parentElement = document.getElementById("contacts-element");
+
+  if (button.classList.contains("fa-plus-square")) {
+    button.classList.remove("fa-plus-square");
+    button.classList.add("fa-minus-square");
+    parentElement.innerHTML = "\"contacts\": { <button type=\"button\" id=\"contacts-minimizable-button\" onclick=\"minimizeContacts()\" class=\"far fa-minus-square json-button\"></button>";
+  } else if (button.classList.contains("fa-minus-square")){
+    button.classList.remove("fa-minus-square");
+    button.classList.add("fa-plus-square");
+    parentElement.innerHTML = "\"contacts\": { <button type=\"button\" id=\"contacts-minimizable-button\" onclick=\"minimizeContacts()\" class=\"far fa-plus-square json-button\"></button> }";
+  }
+  toggleMinimizeElements("minimizable-contacts")
+}
+
+function toggleMinimizeElements(className) {
+  var elements = document.getElementsByClassName(className);
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].style.display !== "none") {
+      elements[i].style.display = "none";
+    } else {
+      elements[i].style.display = "block";
+    }
+  }
+}
+
