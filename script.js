c8b9176be9f2557eddf05181e5d6f632415c3cb9
linusaxel <46885991+linusaxel@users.noreply.github.com>
2021-07-05 13:11:19 +0200

    change stuff

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-var text = `{
+var linusJSON = `{
   "name": "Linus Eriksson",
   "profession": "Software Engineer",
-  "years_of_experience": 2,
-  "isNinja": true,
   "hobbies": ["Coding", "Guitar", "Cats"],
   "contact": {
-    "e-mail": "linusaxel@gmail.com",
-    "github": "link",
-    "linkedin": "link",
-    "twitter": "link"
+      "e-mail": "linusaxel@gmail.com",
+      "github": "https://github.com/linusaxel",
+      "linkedin": "https://www.linkedin.com/in/linusaxel/",
+      "twitter": "https://twitter.com/l3nnybob"
   }
 }`;
+parsedJSON = JSON.parse(linusJSON);
+
+renderjson.set_icons('+', '-')
+  .set_show_to_level(1)
+  .set_collapse_msg(() => {
+      return '';
+  });
+document.getElementById("json-container").appendChild(
+  renderjson(parsedJSON)
+);
